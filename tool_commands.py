9b9a93bbce46bf8d17b4de509e5ff312ef26059a
玊烨 <207279604@qq.com>
2021-05-26 14:24:58 +0800

    定制通用命令

---
+++ tool_commands.py
@@ -0,0 +1,29 @@
+import os
+
+
+class ToolCommand:
+    @staticmethod
+    def create_folder():
+        folder_paths = ["static", "static\\css", "static\\font", "static\\image", "static\\js", "templates"]
+        for folder_path in folder_paths:
+            if not os.path.exists(folder_path):
+                os.mkdir(folder_path)
+
+    @staticmethod
+    def create_app(app_name):
+        os.system(f"python manage.py startapp {app_name}")
+
+    @staticmethod
+    def run_server():
+        os.system("python manage.py runserver 0.0.0.0:8080")
+
+    def main(self):
+        # self.create_folder()
+
+        # self.create_app("")
+
+        self.run_server()
+
+
+if __name__ == '__main__':
+    ToolCommand().main()
